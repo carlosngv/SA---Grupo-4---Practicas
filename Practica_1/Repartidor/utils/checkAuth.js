7a94d4aabe617d9a9e515d189cbe1494ff060656
Carlos Ng <carlosngva@outlook.com>
2022-12-10 01:31:05 -0600

    Microservicio de repartidor implementado

---
+++ Practica_1/Repartidor/utils/checkAuth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const bearerToken = req.headers['authorization'];
+    //console.log(bearerToken)
+    if (!bearerToken) {
+      global.log.error('Token Not Available');
+      return res.status(403).json({"success": false, "message": "Access denied."});
+    }
+
+    const token = bearerToken.split(' ')[1];
+
+    jwt.verify(token, process.env.SECRET, (error, authData) => {
+      if (error) {
+        global.log.error(error);
+        return res.status(403).json({"success": false, "message": error});
+      } else {
+        req.user = authData;
+        global.log.info('Token Available');
+      }
+    });
+
+    // console.log(req.user)
+    next();
+  } catch (error) {
+    global.log.info(error);
+    res.status(400).json({"success": false, "message": "Invalid token"})
+  }
+};
